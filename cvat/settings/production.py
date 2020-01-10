a712047bf0146c4cdd9f6cd9d803789511631196
Ben Qiu <bqiu@dius.com.au>
2020-01-11 00:41:34 +1100

    wip: update image settings

--- cvat/settings/production.py
+++ cvat/settings/production.py
@@ -11,9 +11,9 @@ INSTALLED_APPS += [
 ]
 
 for key in RQ_QUEUES:
-    RQ_QUEUES[key]['HOST'] = 'cvat_redis'
+    RQ_QUEUES[key]['HOST'] = os.getenv('REDIS_HOST', 'cvat_redis')
 
-CACHEOPS_REDIS['host'] = 'cvat_redis'
+CACHEOPS_REDIS['host'] = os.getenv('REDIS_HOST', 'cvat_redis')
 
 # Django-sendfile:
 # https://github.com/johnsensible/django-sendfile
@@ -25,9 +25,9 @@ SENDFILE_BACKEND = 'sendfile.backends.xsendfile'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'cvat_db',
-        'NAME': 'cvat',
-        'USER': 'root',
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'cvat_db'),
+        'NAME': os.getenv('DB_NAME', 'cvat'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
     }
 }
